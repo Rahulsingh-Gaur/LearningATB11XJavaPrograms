9ff30e2ad813615b2051b19ce839f7f8309f4ba2
RQA <rahulhajri28@gmail.com>
2025-03-06 21:15:29 +0530

    added new print line 4 ram

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -2,6 +2,8 @@ package ex_01_Java_Basic;
 
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        System.out.println("Hello ANU");
     }
 }
