57941807d920bba3d8607e542597b514704b3baf
RQA <rahulhajri28@gmail.com>
2025-03-06 21:28:21 +0530

    Merge remote-tracking branch 'origin/main'

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -4,6 +4,6 @@ public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello ANU");
-        System.out.println("Hello Rahulsingh");
+        System.out.println("Hello Rahulsingh hajari");
     }
 }
