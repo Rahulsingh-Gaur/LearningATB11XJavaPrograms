94b2bceec68086769e47871801465239243c536c
RahulSingh Hajari <84778938+Rahulsingh-Gaur@users.noreply.github.com>
2025-03-06 08:45:51 +0530

    Update HelloWorld.java

--- src/ex_01_Java_Basic/HelloWorld.java
+++ src/ex_01_Java_Basic/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Jai Shree Ganesh");
+        System.out.println("Ganpati bappa morya");
     }
 }
