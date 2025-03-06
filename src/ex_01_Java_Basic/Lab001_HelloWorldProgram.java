0203e4caca41209ecde8d9d3253788f1877d7d41
RQA <rahulhajri28@gmail.com>
2025-03-06 21:34:03 +0530

    update via cmd

--- src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldProgram.java
@@ -4,5 +4,7 @@ public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi");
+        System.out.println("Bye");
+        System.out.println("RANA");
     }
 }
