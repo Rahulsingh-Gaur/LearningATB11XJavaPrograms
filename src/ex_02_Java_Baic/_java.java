c80b50030bc5a2710c540f35b8be07a5a659ee87
RQA <rahulhajri28@gmail.com>
2025-03-10 20:42:38 +0530

    add line two on 10th march by rahul2

--- src/ex_02_Java_Baic/_java.java
+++ src/ex_02_Java_Baic/_java.java
@@ -3,9 +3,9 @@ package ex_02_Java_Baic;
 public class _java {
     public static void main(String[] args) {
         System.out.println("hello");
-//        System.out.print("hello");
+       /* System.out.print("hello");
         System.out.print("rahul");
-//        this is rahuyl code
-
-    }
+       this is rahuyl code
+        */
+        }
 }
