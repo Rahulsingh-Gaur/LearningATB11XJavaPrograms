f415e0708061d3abc029f2696f3c834d8b9e3e3e
RQA <rahulhajri28@gmail.com>
2025-03-10 20:46:05 +0530

    Sout added

--- src/ex_02_Java_Baic/_java.java
+++ src/ex_02_Java_Baic/_java.java
@@ -3,9 +3,14 @@ package ex_02_Java_Baic;
 public class _java {
     public static void main(String[] args) {
         System.out.println("hello");
-       /* System.out.print("hello");
-        System.out.print("rahul");
-       this is rahuyl code
-        */
+        System.out.println("Rahl");
+        System.out.println("singh");
+        System.out.println("hajari");
+
+
+
+
+
+
         }
 }
