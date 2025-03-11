758a8cdf30a944ff9e23d6dfe8ee3ac1b04651a3
RQA <rahulhajri28@gmail.com>
2025-03-11 20:51:28 +0530

    Adding 9 tbale

---
+++ src/ex_02_Java_Basic/Lab004_Print_Nine_Table.java
@@ -0,0 +1,29 @@
+package ex_02_Java_Basic;
+
+public class Lab004_Print_Nine_Table {
+    public static void main(String[] args) {
+//        System.out.println(9*1);
+//        System.out.println(9*2);
+//        System.out.println(9*3);
+//        System.out.println(9*4);
+//        System.out.println(9*5);
+//        System.out.println(9*6);
+//        System.out.println(9*7);
+//        System.out.println(9*8);
+//        System.out.println(9*9);
+//        System.out.println(9*10);
+
+        int table = 9;
+        System.out.println("Printing the 9 table");
+        System.out.printf("9 * 1 : %d%n", (table*1));
+        System.out.printf("9 * 2 : %d%n", (table*2));
+        System.out.printf("9 * 3 : %d%n", (table*3));
+        System.out.printf("9 * 4 : %d%n", (table*4));
+        System.out.printf("9 * 5 : %d%n", (table*5));
+        System.out.printf("9 * 6 : %d%n", (table*6));
+        System.out.printf("9 * 7 : %d%n", (table*7));
+        System.out.printf("9 * 8 : %d%n", (table*8));
+        System.out.printf("9 * 9 : %d%n", (table*9));
+        System.out.printf("9 * 10 : %d%n", (table*10));
+    }
+}
