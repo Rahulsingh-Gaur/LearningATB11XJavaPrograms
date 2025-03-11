0619673c00108bc11b1eab8526d38f8c3dbd15dd
RQA <rahulhajri28@gmail.com>
2025-03-11 23:11:59 +0530

    rahul added Bnak sms

--- src/ex_02_Java_Basic/Lab004_Print_Nine_Table.java
+++ src/ex_02_Java_Basic/Lab004_Print_Nine_Table.java
@@ -13,17 +13,18 @@ public class Lab004_Print_Nine_Table {
 //        System.out.println(9*9);
 //        System.out.println(9*10);
 
-        int table = 9;
+        int PrintTable = 9;
         System.out.println("Printing the 9 table");
-        System.out.printf("9 * 1 : %d%n", (table*1));
-        System.out.printf("9 * 2 : %d%n", (table*2));
-        System.out.printf("9 * 3 : %d%n", (table*3));
-        System.out.printf("9 * 4 : %d%n", (table*4));
-        System.out.printf("9 * 5 : %d%n", (table*5));
-        System.out.printf("9 * 6 : %d%n", (table*6));
-        System.out.printf("9 * 7 : %d%n", (table*7));
-        System.out.printf("9 * 8 : %d%n", (table*8));
-        System.out.printf("9 * 9 : %d%n", (table*9));
-        System.out.printf("9 * 10 : %d%n", (table*10));
+        System.out.printf("9 * 1 : %d%n", (PrintTable*1));
+        System.out.printf("9 * 2 : %d%n", (PrintTable*2));
+        System.out.printf("9 * 3 : %d%n", (PrintTable*3));
+        System.out.printf("9 * 4 : %d%n", (PrintTable*4));
+        System.out.printf("9 * 5 : %d%n", (PrintTable*5));
+        System.out.printf("9 * 6 : %d%n", (PrintTable*6));
+        System.out.printf("9 * 7 : %d%n", (PrintTable*7));
+        System.out.printf("9 * 8 : %d%n", (PrintTable*8));
+        System.out.printf("9 * 9 : %d%n", (PrintTable*9));
+        System.out.printf("9 * 10 :%d%n", (PrintTable*10));
+
     }
 }
---
+++ src/ex_02_Java_Basic/Lab_004_Banksms_printf.java
@@ -0,0 +1,14 @@
+package ex_02_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Lab_004_Banksms_printf {
+    public static void main(String[] args) {
+        String  Name = "Rahulsingh Gaur";
+        String  Bank= "SBI BANK !";
+        double  acc_no = 20000454566567d;
+        int Amount = 7500;
+
+        System.out.printf("Hello Mr. %s , RS %d is Credit in %.0f ,Thank you so much for banking with us %s ", Name,Amount,acc_no,Bank);
+    }
+}
