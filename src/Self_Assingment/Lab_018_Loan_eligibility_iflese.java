c76306b9c59eaf80d0c7735c6baf3890d3a00289
RQA <rahulhajri28@gmail.com>
2025-03-19 00:12:32 +0530

    19  march assigment

---
+++ src/Self_Assingment/Lab_018_Loan_eligibility_iflese.java
@@ -0,0 +1,39 @@
+package Self_Assingment;
+
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class Lab_018_Loan_eligibility_iflese {
+
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Enter the Age !");
+        int age = scanner.nextInt();
+
+        System.out.println("Enter the income");
+        float income =scanner.nextFloat();
+
+        System.out.println("Enter the Credit Score");
+        float Cs =scanner.nextFloat();
+
+        if (age>=21){
+            if (income>=30000){
+                 if(Cs>=650){
+                     System.out.println("congratulations ! You are eligibile for loan");
+                 }
+                 else System.out.println("Sorry !You are Not eligibile for loan ");
+            }
+            else System.out.println("Sorry !You are Not eligibile for loan ");
+        }
+        else System.out.println("Sorry !You are Not eligibile for loan ");
+
+    }
+}
+
+//Create a program that asks the user for their age, annual income, and credit score. Use if-else statements to determine loan eligibility:
+//
+//    Age: Must be 21 or older
+//    Income: Must be above $30,000
+//    Credit Score: Must be above 650
