97fb9b0e80c46c78f23d8459a6b9c10062d0aed2
RQA <rahulhajri28@gmail.com>
2025-03-24 08:19:32 +0530

    24  march assigment

--- src/JAVA_11X_Task/Lab013_Palimdorme.java
+++ src/JAVA_11X_Task/Lab013_Palimdorme.java
@@ -6,18 +6,21 @@ import java.util.Scanner;
 public class Lab013_Palimdorme {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the name");
+        System.out.println("Enter the name !!!");
         String name = scanner.nextLine();
-        String Result = Str(name);
-        for (int i =0 ; i<name.length();i++){
-            char ch = name.charAt(i);
+        int Length =name.length();
+        String Rev = new StringBuilder(name).reverse().toString();
+        System.out.println(Rev);
+
+        if (name.equals(Rev)){
+            System.out.println("THis is Palidrome");
+        }
+        else {
+            System.out.println("THis is not Palidrome");
+        }
 
         }
 
-
-
-
-
     }
-}
+
 //✅ Palidrome of String (chatAt()) - naman, → reverse - naman , madam ( string = rev(str)
